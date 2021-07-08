6f89ea6ad0d467ef9f2b56af2bdcdd005911def0
Obliviated <hamzacoskun41@gmail.com>
2021-07-08 12:33:38 +0300

    Removed Bukkit API.

--- src/main/java/mc/obliviate/bloksqliteapi/SQLHandler.java
+++ src/main/java/mc/obliviate/bloksqliteapi/SQLHandler.java
@@ -1,7 +1,5 @@
 package mc.obliviate.bloksqliteapi;
 
-import org.bukkit.plugin.Plugin;
-
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,10 +10,10 @@ public class SQLHandler {
 
 	private static Statement statement;
 	private static Connection connection;
-	private final Plugin plugin;
+	private final String filePath;
 
-	public SQLHandler(final Plugin plugin) {
-		this.plugin = plugin;
+	public SQLHandler(final String filePath) {
+		this.filePath = filePath;
 	}
 
 	public static Connection getConnection() {
@@ -29,7 +27,7 @@ public class SQLHandler {
 	public void connect(final String databaseName) {
 		connection = null;
 		try {
-			final String URL = "jdbc:sqlite:" + plugin.getDataFolder().getAbsolutePath() + File.separator + databaseName + ".db";
+			final String URL = "jdbc:sqlite:" + filePath + File.separator + databaseName + ".db";
 			connection = DriverManager.getConnection(URL);
 			statement = connection.createStatement();
 			onConnect();
@@ -49,61 +47,4 @@ public class SQLHandler {
 	public void onConnect() {
 
 	}
-	/**
-	public SQLTable createTable() {
-
-		//Create a table object
-		//First param, specify your table name
-		//Second param, specify name of field that you'll use instead of ID
-		//Third param, add a field to your table.
-		//You know. Fields has some properties, data type, not null, unique, primary key...
-		//You can specify this properties too.
-		final SQLTable sqlTable = new SQLTable("school3", "no")
-				.addField("no", DataType.INTEGER, true, true, true)
-				.addField("name", DataType.TEXT) //other params is false as default
-				.addField("class", DataType.TEXT);
-
-		//Create table
-		//No No No. You don't need the long text. You already given what we
-		//need so we can create table instead of you.
-		//
-		//just use sqlTable.create() and enjoy.
-		//this method returns created table.
-		return sqlTable.create();
-
-	}
-	public void test(SQLTable sqltable) {
-		Bukkit.getLogger().info("Test starting...");
-		final long startTime = System.currentTimeMillis();
-
-		//Let's test our database.
-		//firstly specify an id to make an example. i choose 306. (because that is my school number xd)
-
-		int id = 306;
-
-		//Check your table and ask 'is this id inserted before?'
-		if (sqltable.exist(id)) {
-
-			//if answer is yes, you must update.
-			//your table already has another fields you need.
-			//so, you can specify fields only that you want to change.
-			sqltable.update(sqltable.createUpdate(id).putData("name", "ali"));
-		} else {
-
-			//if answer is no, you must insert.
-			//your table does not have another fields you need.
-			//so you must specify all fields.
-			sqltable.insert(sqltable.createUpdate(id).putData("name", "hamza").putData("class", "13E").putData("no", id));
-		}
-
-		sqltable.delete(308);
-		//sqltable.insertOrUpdate(sqltable.createUpdate(308).putData("name","mehmet").putData("class","12A").putData("no",308));
-
-		final long endTime = System.currentTimeMillis();
-		final long totalTime = endTime - startTime;
-		Bukkit.getLogger().info("Test completed (" + totalTime + "ms)");
-	}
-
-    **/
-
 }
